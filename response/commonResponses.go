40df32f78464eb1345d709da6f28cde4eeaaca8c
habit <habitnature@outlook.com>
2021-12-17 00:33:39 +0800

    feature : traceId-> uuid

--- response/commonResponses.go
+++ response/commonResponses.go
@@ -7,25 +7,25 @@ import (
 )
 
 func OkResult(ctx *gin.Context, code int, data interface{}, msg string) {
-	ctx.JSONP(http.StatusOK, gin.H{"code": code, "data": data, "msg": msg, "traceId": utils.NewUUID()})
+	ctx.JSONP(http.StatusOK, gin.H{"code": code, "data": data, "msg": msg, "uuid": utils.NewUUID()})
 }
 
 func OkDataMsg(ctx *gin.Context, data interface{}, msg string) {
-	ctx.JSONP(http.StatusOK, gin.H{"code": CodeOk, "data": data, "msg": msg, "traceId": utils.NewUUID()})
+	ctx.JSONP(http.StatusOK, gin.H{"code": CodeOk, "data": data, "msg": msg, "uuid": utils.NewUUID()})
 }
 
 func Ok(ctx *gin.Context, data interface{}) {
-	ctx.JSONP(http.StatusOK, gin.H{"code": CodeOk, "data": data, "msg": nil, "traceId": utils.NewUUID()})
+	ctx.JSONP(http.StatusOK, gin.H{"code": CodeOk, "data": data, "msg": nil, "uuid": utils.NewUUID()})
 }
 
 func ErrorResult(ctx *gin.Context, code int, msg string) {
-	ctx.JSONP(http.StatusOK, gin.H{"code": code, "data": nil, "msg": msg, "traceId": utils.NewUUID()})
+	ctx.JSONP(http.StatusOK, gin.H{"code": code, "data": nil, "msg": msg, "uuid": utils.NewUUID()})
 }
 
 func ErrorMsg(ctx *gin.Context, msg string) {
-	ctx.JSONP(http.StatusOK, gin.H{"code": CodeError, "data": nil, "msg": msg, "traceId": utils.NewUUID()})
+	ctx.JSONP(http.StatusOK, gin.H{"code": CodeError, "data": nil, "msg": msg, "uuid": utils.NewUUID()})
 }
 
 func Error(ctx *gin.Context, err error) {
-	ctx.JSONP(http.StatusOK, gin.H{"code": CodeError, "data": nil, "msg": err.Error(), "traceId": utils.NewUUID()})
+	ctx.JSONP(http.StatusOK, gin.H{"code": CodeError, "data": nil, "msg": err.Error(), "uuid": utils.NewUUID()})
 }
